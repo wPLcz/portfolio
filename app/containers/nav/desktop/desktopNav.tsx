9fd5a0705d1ca833eb6071ff200fd512ea7e73dc
Wojciech Palacz <wojciechpalacz@gmail.com>
2024-01-19 20:59:55 +0100

    adding animation

--- app/containers/nav/desktop/desktopNav.tsx
+++ app/containers/nav/desktop/desktopNav.tsx
@@ -1,17 +1,17 @@
 'use client'
+import { menuListItems } from '@/app/config/menuListItems.ts';
+import { Logo } from '@/components/layout/logo/logo.tsx';
+import { FacebookIcon } from '@/components/layout/socialMedia/facebookIcon/FacebookIcon.tsx';
+import { GithubIcon } from '@/components/layout/socialMedia/githubIcon/GithubIcon.tsx';
+import { LinkedInIcon } from '@/components/layout/socialMedia/linkedinIcon/LinkedInIcon.tsx';
+import { Link } from '@/components/ui/link/link.tsx';
+import classNames from 'classnames';
 import { usePathname } from 'next/navigation';
 import { FC } from 'react';
 import styles from './desktopNav.module.scss';
-import { Logo } from '@/components/layout/logo/logo.tsx';
-import { LinkedInIcon } from '@/components/layout/socialMedia/linkedinIcon/LinkedInIcon.tsx';
-import { GithubIcon } from '@/components/layout/socialMedia/githubIcon/GithubIcon.tsx';
-import { FacebookIcon } from '@/components/layout/socialMedia/facebookIcon/FacebookIcon.tsx';
-import { menuListItems } from '@/app/config/menuListItems.ts';
-import { Link } from '@/components/ui/link/link.tsx';
-import classNames from 'classnames';
 
 export const DesktopNav: FC = () => {
-  const router = usePathname();
+  const router = usePathname()
 
   return (
     <aside className={styles.aside}>
@@ -31,15 +31,15 @@ export const DesktopNav: FC = () => {
         </a>
       </div>
 
-      <nav className={styles.navigation}>
-        <ul className={styles.asideList}>
+      <nav className={styles.navigationWrapper}>
+        <ul className={styles.nav}>
           {menuListItems.map((item, index) => {
             const { href, label } = item;
             const isActive = router.endsWith(href);
             return (
               <li
                 className={classNames(
-                  styles.asideListItem,
+                  styles.navItem,
                   { [styles.active]: isActive }
                 )}
                 key={item.href + index}>
--- app/containers/nav/mobile/mobileNav.tsx
+++ app/containers/nav/mobile/mobileNav.tsx
@@ -1,12 +1,29 @@
 'use client'
-import { Menu } from '@/components/layout/menu/menu.tsx';
-import { FC, useState } from 'react';
-import styles from './mobileNav.module.scss';
+import { menuListItems } from '@/app/config/menuListItems.ts';
 import { Logo } from '@/components/layout/logo/logo.tsx';
+import { Menu } from '@/components/layout/menu/menu.tsx';
+import { FacebookIcon } from '@/components/layout/socialMedia/facebookIcon/FacebookIcon.tsx';
+import { GithubIcon } from '@/components/layout/socialMedia/githubIcon/GithubIcon.tsx';
+import { LinkedInIcon } from '@/components/layout/socialMedia/linkedinIcon/LinkedInIcon.tsx';
+import { Link } from '@/components/ui/link/link.tsx';
 import classNames from 'classnames';
+import { usePathname } from 'next/navigation';
+import { FC, useEffect, useState } from 'react';
+import styles from './mobileNav.module.scss';
 
 export const MobileNav: FC = () => {
+  const router = usePathname();
   const [isOpen, setIsOpen] = useState<boolean>(false);
+
+
+  useEffect(() => {
+    document.addEventListener(
+      'resize',
+      () => {
+        setIsOpen(false);
+      }
+    )
+  });
 
   return (
     <aside
@@ -14,13 +31,56 @@ export const MobileNav: FC = () => {
         styles.aside,
         { [styles.open]: isOpen }
       )}>
-      <div className={styles.logo}>
-        <Logo/>
-      </div>
 
-      <div className={styles.menu}>
-        <Menu
-          isOpen={isOpen} toggle={(value) => setIsOpen(value)}/>
+      <div className={styles.socialIcons}>
+        <a href='https://github.com/wPLcz' className={styles.socialIconAnchor}>
+          <GithubIcon/>
+        </a>
+        <a href='https://www.facebook.com/wojciech.k.palacz/' className={styles.socialIconAnchor}>
+          <LinkedInIcon/>
+        </a>
+        <a href='https://www.linkedin.com/in/wojciech-palacz/' className={styles.socialIconAnchor}>
+          <FacebookIcon/>
+        </a>
+      </div>
+      )
+
+      <nav>
+        <ul className={styles.nav}>
+          {menuListItems.map((item, index) => {
+            const { href, label } = item;
+            const isActive = router.endsWith(href);
+            return (
+              <li
+                className={classNames(
+                  styles.navItem,
+                  styles['navItem' + index],
+                  { [styles.active]: isActive }
+                )}
+                key={item.href + index}>
+                <Link
+                  href={href}
+                  label={label}
+                  size={'large'}
+                  target={item.isDownloadable ? '_blank' : undefined}
+                  isActive={isActive}
+                  underline
+                />
+              </li>
+            )
+          })}
+        </ul>
+      </nav>
+
+      <div className={styles.main}>
+        <div className={styles.logo}>
+          <Logo/>
+        </div>
+
+        <div className={styles.menu}>
+          <Menu
+            isOpen={isOpen} toggle={(value) => setIsOpen(value)}/>
+        </div>
       </div>
 
     </aside>
