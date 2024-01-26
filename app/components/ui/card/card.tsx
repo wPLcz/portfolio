ebe34d156da4062b01f011cf1f4ca70c79c2bf45
Wojciech Palacz <wojciechpalacz@gmail.com>
2024-01-26 08:32:08 +0100

    adding colorful animated borders

--- app/components/ui/card/card.tsx
+++ app/components/ui/card/card.tsx
@@ -2,7 +2,7 @@ import { CardProps } from '@/app/components/ui/card/card.types';
 import { FC } from 'react';
 import styles from './card.module.scss';
 
-export const Card: FC<CardProps> = ({ children, image }) => {
+export const Card: FC<CardProps> = ({ children }) => {
   return (
     <div className={styles.container}>
       <div className={styles.content}>
